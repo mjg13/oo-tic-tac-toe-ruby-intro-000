2e987f38d5ed7e9c59f49325922cf388a2b45cdc
Mary Jo Gray <mj81000@gmail.com>
2021-01-15 19:49:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,12 +19,12 @@ class TicTacToe
     input.to_i-1
   end
 
-  def position_taken?(board, location)
-    board[location] != " "
+  def position_taken?(location)
+    @board[location] != " "
   end
 
-  def valid_move?(board, index)
-    index.between?(0,8) && !position_taken?(board, index)
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
   end
 
   def current_player
